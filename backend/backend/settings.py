d59dac98dcd9b03543e4e638bb1ee66c3ebbdf0f
Imad Afif <imadafif.00@gmail.com>
2024-12-09 16:16:49 +0100

    ;)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'EMP',
+    'auth'
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'auth.User'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('EMP.urls')),  
+    path('api/', include('EMP.urls')),
+    path('api/', include('auth.urls')) 
 ]
