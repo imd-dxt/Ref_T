90ae452c26494e33227f97ec161ee0e55c2e9915
Imad Afif <imadafif.00@gmail.com>
2024-12-14 22:24:39 +0100

    :-)

--- backend/EMP/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import BankAccount
-from .models import employee , Client
-admin.site.register(employee)
-admin.site.register(BankAccount)
-admin.site.register(Client)
-# Register your models here.
